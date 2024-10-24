441a3c44817b934c836422375124bb4c5aa37cff
PepijnTEW <37923@ma-web.nl>
2024-10-24 08:45:37 +0200

    les 3 include require

---
+++ views/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer mt-auto py-3 bg-body-tertiary">
+    <script src="/dist/js/main.js"></script>
+    <div class="container">
+        <span class="text-body-secondary">Place footer content here.</span>
+    </div>
+</footer>
